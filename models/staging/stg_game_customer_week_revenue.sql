ead827c6ee5ba072db58492f0aa9a37151ebf605
TheDonumEstate <nathan@thedonumestate.com>
2025-08-29 12:10:44 -0700

    Reorganizing warehouse views

---
+++ models/staging/stg_game_customer_week_revenue.sql
@@ -0,0 +1,12 @@
+-- Customer-week revenue (net: excluding tax, shipping, tip)
+SELECT
+  fo.customer_id,
+  d.fiscal_year,
+  d.fiscal_week_of_year as fiscal_week,
+  sum(fo.sub_total_cents)/100.0
+  - sum(coalesce(fo.tax_total_cents,0))/100.0
+  - sum(coalesce(fo.ship_total_cents,0))/100.0
+  - sum(coalesce(fo.tip_total_cents,0))/100.0 as net_revenue
+FROM fact_order fo
+JOIN dim_fiscal_date d ON date(fo.submitted_at) = d.calendar_date
+GROUP BY 1,2,3;
---
+++ models/staging/stg_game_fiscal_weeks.sql
@@ -0,0 +1,8 @@
+-- Helpful context: week start/end (for UI/context)
+SELECT
+  fiscal_year,
+  fiscal_week_of_year as fiscal_week,
+  min(calendar_date) as week_start,
+  max(calendar_date) as week_end
+FROM dim_fiscal_date
+GROUP BY 1,2;
