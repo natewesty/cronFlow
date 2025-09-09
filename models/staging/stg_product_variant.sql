d98cea3201820c00f26948aae545a1aa372dc250
TheDonumEstate <nathan@thedonumestate.com>
2025-09-09 13:41:15 -0700

    Another syntax update for sub_title
    Updated stg_product_variant.sql to use sub_title directly

--- models/staging/stg_product_variant.sql
+++ models/staging/stg_product_variant.sql
@@ -14,7 +14,7 @@ variant as (
         product_id,
         (v->>'id')::uuid                      as variant_id,
         v->>'title'                           as variant_title,
-        base.sub_title                        as sub_title,
+        sub_title,
         v->>'sku'                             as sku,
         v->>'inventoryPolicy'                 as inventory_policy,
         v->>'taxType'                         as tax_type,
