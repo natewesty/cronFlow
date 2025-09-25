3632a88373d260176c98bb0c7f686daaba7ded56
TheDonumEstate <nathan@thedonumestate.com>
2025-09-25 14:28:54 -0700

    Updated Tock staging models to use correct JSONB data types.

--- models/staging/stg_tock_guest.sql
+++ models/staging/stg_tock_guest.sql
@@ -11,42 +11,42 @@ with source_data as (
 parsed_data as (
     select
         id::varchar as guest_id,
-        data->>'id'::varchar as tock_guest_id,
-        data->'patron'->>'id'::varchar as patron_id,
-        data->'patron'->>'email'::varchar as email,
-        data->'patron'->>'firstName'::varchar as first_name,
-        data->'patron'->>'lastName'::varchar as last_name,
-        data->'patron'->>'phone'::varchar as phone,
-        data->'patron'->>'phoneCountryCode'::varchar as phone_country_code,
-        data->'patron'->>'zipCode'::varchar as zip_code,
-        data->'patron'->>'isoCountryCode'::varchar as iso_country_code,
-        data->'address'->>'country'::varchar as address_country,
+        data->>'id' as tock_guest_id,
+        data->'patron'->>'id' as patron_id,
+        data->'patron'->>'email' as email,
+        data->'patron'->>'firstName' as first_name,
+        data->'patron'->>'lastName' as last_name,
+        data->'patron'->>'phone' as phone,
+        data->'patron'->>'phoneCountryCode' as phone_country_code,
+        data->'patron'->>'zipCode' as zip_code,
+        data->'patron'->>'isoCountryCode' as iso_country_code,
+        data->'address'->>'country' as address_country,
         
         -- Extract phone array data
         jsonb_array_length(data->'phone') as phone_count,
-        data->'phone'->0->>'phone'::varchar as primary_phone,
-        data->'phone'->0->>'phoneCountryCode'::varchar as primary_phone_country_code,
+        data->'phone'->0->>'phone' as primary_phone,
+        data->'phone'->0->>'phoneCountryCode' as primary_phone_country_code,
         
         -- Extract day array data (birthdays, anniversaries, etc.)
         jsonb_array_length(data->'day') as day_count,
         case 
             when jsonb_array_length(data->'day') > 0 
-            then data->'day'->0->>'type'::varchar 
+            then data->'day'->0->>'type'
             else null 
         end as primary_day_type,
         case 
             when jsonb_array_length(data->'day') > 0 
-            then data->'day'->0->>'day'::int 
+            then (data->'day'->0->>'day')::int 
             else null 
         end as primary_day,
         case 
             when jsonb_array_length(data->'day') > 0 
-            then data->'day'->0->>'month'::int 
+            then (data->'day'->0->>'month')::int 
             else null 
         end as primary_month,
         case 
             when jsonb_array_length(data->'day') > 0 
-            then data->'day'->0->>'year'::int 
+            then (data->'day'->0->>'year')::int 
             else null 
         end as primary_year,
         
@@ -54,12 +54,12 @@ parsed_data as (
         jsonb_array_length(data->'link') as link_count,
         case 
             when jsonb_array_length(data->'link') > 0 
-            then data->'link'->0->>'type'::varchar 
+            then data->'link'->0->>'type'
             else null 
         end as primary_link_type,
         case 
             when jsonb_array_length(data->'link') > 0 
-            then data->'link'->0->>'link'::varchar 
+            then data->'link'->0->>'link'
             else null 
         end as primary_link_url,
         
@@ -72,14 +72,14 @@ parsed_data as (
         data->'patronProfileHospitalityPreference' as hospitality_preferences,
         
         -- Extract aversions
-        data->>'patronProfileAversions'::varchar as aversions,
+        data->>'patronProfileAversions' as aversions,
         
         -- Business and metadata
-        data->>'businessGroupId'::int as business_group_id,
-        data->>'optInSource'::varchar as opt_in_source,
-        data->>'isArchived'::boolean as is_archived,
-        data->>'canEdit'::boolean as can_edit,
-        data->>'isTockVerified'::boolean as is_tock_verified,
+        (data->>'businessGroupId')::int as business_group_id,
+        data->>'optInSource' as opt_in_source,
+        (data->>'isArchived')::boolean as is_archived,
+        (data->>'canEdit')::boolean as can_edit,
+        (data->>'isTockVerified')::boolean as is_tock_verified,
         
         -- Timestamps
         to_timestamp((data->>'createdAtTimestamp')::bigint / 1000) as created_at,
