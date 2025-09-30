ba488c6ef83057f5b978211f6589c6012c1f9eb2
TheDonumEstate <nathan@thedonumestate.com>
2025-09-30 14:03:37 -0700

    Adding in new mart for tock reservations. Data is usable for metric calculations.

---
+++ models/marts/fct_tock_reservation.sql
@@ -0,0 +1,66 @@
+{{
+  config(
+    materialized='table'
+  )
+}}
+
+with source_data as (
+    select * from {{ ref('stg_tock_reservation') }}
+),
+
+payment_data as (
+    select
+        tock_reservation_id,
+        -- Extract payment information from the payments JSON array
+        case 
+            when jsonb_array_length(payments) > 0 
+            then (payments->0->>'tockFeeCents')::int 
+            else 0 
+        end as tock_fee_cents,
+        case 
+            when jsonb_array_length(payments) > 0 
+            then (payments->0->>'processorFeeCents')::int 
+            else 0 
+        end as processor_fee_cents
+    from source_data
+),
+
+reservation_mart as (
+    select
+        -- Basic reservation information
+        tock_reservation_id,
+        
+        -- Diner patron information (renamed columns)
+        diner_patron_id as patron_id,
+        diner_patron_first_name as first_name,
+        diner_patron_last_name as last_name,
+        diner_patron_email as email,
+        
+        -- Reservation details
+        to_char(
+            to_timestamp(reservation_datetime, 'YYYY-MM-DD"T"HH24:MI:SS"Z"'), 
+            'MM-DD-YYYY'
+        ) as reservation_datetime,
+        party_size,
+        experience_name,
+        
+        -- Pricing information (converted from cents to dollars)
+        round(subtotal_cents / 100.0, 2) as subtotal,
+        
+        -- Payment fees (converted from cents to dollars)
+        round(pd.tock_fee_cents / 100.0, 2) as tock_fee,
+        round(pd.processor_fee_cents / 100.0, 2) as processor_fee,
+        
+        -- Calculate final total
+        round(
+            (subtotal_cents / 100.0) + 
+            (pd.tock_fee_cents / 100.0) + 
+            (pd.processor_fee_cents / 100.0), 
+            2
+        ) as final_total
+        
+    from source_data sd
+    left join payment_data pd on sd.tock_reservation_id = pd.tock_reservation_id
+)
+
+select * from reservation_mart
