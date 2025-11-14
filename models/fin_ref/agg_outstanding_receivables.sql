95add7dcb1cde4cdea1d931654403795ba4cf706
TheDonumEstate <nathan@thedonumestate.com>
2025-11-14 13:38:31 -0800

    Update outstanding receivables to include month name in display time

--- models/fin_ref/agg_outstanding_receivables.sql
+++ models/fin_ref/agg_outstanding_receivables.sql
@@ -19,15 +19,18 @@ with monthly_data as (
 )
 select
     month_end_date_paid as transaction_date,
+    concat(extract(year from month_end_date_paid), ' - ', month_name) as display_time,
     max(class_code) as class_code,
     sku as item,
     round(sum(quantity)::numeric / max(case_size)::numeric, 5) as quantity,
-    unit_of_measure
+    unit_of_measure,
+    (round(sum(quantity)::numeric / max(case_size)::numeric, 5) * max(extrapolated_price))::money as value_on_street
 from monthly_data
 group by
     month_end_date_paid,
+    month_name,
     sku,
     unit_of_measure
 having round(sum(quantity)::numeric / max(case_size)::numeric, 5) != 0
-order by month_end_date_paid desc, class_code, sku
+order by month_end_date_paid desc, month_name, class_code, sku
 
